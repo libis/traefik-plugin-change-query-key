92395743f3eb03056ab0e0c3d1783b045a6cb72a
Seth Linn <seth.linn@kuleuven.be>
2023-11-27 09:51:57 +0100

    Add createifmissing functionality
    If the parameter is not there it need to be created.

--- change-query-key.go
+++ change-query-key.go
@@ -1,4 +1,3 @@
-package traefik_plugin_change_query_key
 
 import (
 	"context"
@@ -7,38 +6,56 @@ import (
 )
 
 type changeQueryKey struct {
-	keyName    string
-	newKeyName string
-	next       http.Handler
-	name       string
-	config     *Config
+	keyName         string
+	newKeyName      string
+	createIfMissing bool
+	value           string
+	next            http.Handler
+	name            string
+	config          *Config
 }
 
 // Config holds configuration to be passed to the plugin.
 type Config struct {
-	KeyName    string `json:"keyname"`
-	NewKeyName string `json:"newkeyname"`
+	KeyName         string `json:"keyname"`
+	NewKeyName      string `json:"newkeyname"`
+	CreateIfMissing bool   `json:"createifmissing"`  // If true, create a new key if it doesn't exist. Then value can not be empty
+	Value           string `json:"value"`            // The value to set the new key to. Can't be empty if createifmissing is true.
 }
 
 // CreateConfig populates the Config data object.
 func CreateConfig() *Config {
 	return &Config{
-		KeyName:    "",
-		NewKeyName: "",
+		KeyName:         "",
+		NewKeyName:      "",
+		CreateIfMissing: false,
+		Value:           "",
 	}
 }
 
 func (q *changeQueryKey) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 
-	value := r.URL.Query().Get(q.keyName)
+	// Get the value associated with the specified key (q.keyName) in the original query parameters.
+	value := query.Get(q.keyName)
 
-	//Delete the old value and add a new one.
-	query.Del(q.keyName)
-	query.Add(q.newKeyName, value)
+	// Check if the original key exists in the query parameters.
+	if value != ""  {
+		// Delete the original key from the query parameters.
+		query.Del(q.keyName)
+		// Add a new key (q.newKeyName) with the value obtained from the original key.
+		query.Add(q.newKeyName, value)
+	} else if q.createIfMissing {
+		// If the original key doesn't exist, and createifmissing is set, we need to add a new key with a value.
+		value = q.value
+		query.Add(q.newKeyName, value)
+	}
 
+	// Encode the modified query parameters and update the request URL and request URI.
 	r.URL.RawQuery = query.Encode()
 	r.RequestURI = r.URL.RequestURI()
+
+	// Call the ServeHTTP method of the next HTTP handler in the chain (q.next) with the modified request.
 	q.next.ServeHTTP(rw, r)
 }
 
@@ -47,11 +64,18 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 		return nil, errors.New("keyname and newkeyname must be specified in configuration")
 	}
 
+	// If createifmissing is set, we need to have a value.
+	if config.CreateIfMissing && config.Value == "" {
+		return nil, errors.New("createifmissing is set, but value is empty")
+	}
+
 	return &changeQueryKey{
-		keyName:    config.KeyName,
-		newKeyName: config.NewKeyName,
-		next:       next,
-		name:       name,
-		config:     config,
+		keyName:         config.KeyName,
+		newKeyName:      config.NewKeyName,
+		createIfMissing: config.CreateIfMissing,
+		value:           config.Value,
+		next:            next,
+		name:            name,
+		config:          config,
 	}, nil
 }
